b83819f26aaca2c2ec8f9701b1126571fa202bb7
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-05-26 09:37:58 +0500

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+
 import { useSelector, useDispatch } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
+import Users from "./components/user";
 
 function App() {
-  const dispatch = useDispatch();
-  const { cartItems } = useSelector((store) => store.cart);
-  useEffect(() => {
-    dispatch(calculateTotals());
-  }, [cartItems]);
   return (
     <main>
-      <Navbar />
-      <CartContainer />
+      <Users />
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import { store } from "./store";
+import store from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
